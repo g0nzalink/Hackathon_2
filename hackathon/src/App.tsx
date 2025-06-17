ccff7829d8128e4701275ed1ed97f94fe0c16fbf
Gonzalink <gonzalink1907@gmail.com>
2025-06-17 16:52:29 -0500

    correcciones pequenias

---
+++ hackathon/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return(
+    <div>
+      <h1>hola mundo</h1>
+      <p>verificando que todo funcione</p>
+    </div>
+  );
+}
+
+export default App
